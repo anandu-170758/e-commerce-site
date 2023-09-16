9cb5dacc9922c8c7de9979dd18139d92935416d3
anandu_170758 <ananduksyam@gmail.com>
2023-09-16 23:31:43 +0530

    useEffect added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [results,SetResults]=useState([]);
+  React.useEffect(()=>{
+    fetch("http://localhost:3001/categories")
+    .then(response=>response.json())
+    .then(data=>{
+      console.log(data);
+      SetResults(data);
+    })
+  },[])
   return (
     <div className="App">
      HI
