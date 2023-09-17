e53c3fa328e2da33f3d634121237e065245f8e69
anandu_170758 <ananduksyam@gmail.com>
2023-09-17 12:37:21 +0530

    Rendered the Category

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Category from "./Components/Category";
 
 function App() {
-  const [results,SetResults]=useState([]);
-  React.useEffect(()=>{
+  const [results, SetResults] = useState([]);
+  React.useEffect(() => {
     fetch("http://localhost:3001/categories")
-    .then(response=>response.json())
-    .then(data=>{
-      console.log(data);
-      SetResults(data);
-    })
-  },[])
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        SetResults(data);
+      });
+  }, []);
+  const renderCategories = () => {
+    const categories=[];
+    for(let i=0;i<results.length;i++){
+      categories.push(<Category key={results[i].id} id={results[i].id} title={results[i].tittle}/>
+      )
+     
+    }
+    return categories ;
+    
+    
+  };
+
   return (
     <>
-    <header>My Store</header>
-    <section>
-<nav>
-    {
-    results.map(d=>(
-      <div key={d.id}>{d.tittle}</div>
-    ))}
-    </nav>
-    <article>
-      main area
-    </article>
-    </section>
-    <footer>
-      footer
-    </footer>
-   
+      <header>My Store</header>
+      <section>
+        <nav>{results && renderCategories()}</nav>
+        <article>main area</article>
+      </section>
+      <footer>footer</footer>
     </>
   );
 }
